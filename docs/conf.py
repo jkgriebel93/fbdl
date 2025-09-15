40126ff2c666a789ae0dc9856453ce6395b33964
John Griebel <jkgriebel93@gmail.com>
2025-09-15 16:36:45 -0400

    Use alabaster theme for now, hopefully fixing rendering issues
    Signed-off-by: John Griebel <jkgriebel93@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -60,5 +60,5 @@ exclude_patterns = [
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
 html_static_path = ["_static"]
